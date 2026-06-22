366f8a5c698f984a124013917f7d979f30ca1377
AHartNtkn <soobtoob@gmail.com>
2026-06-21 23:50:25 -0400

    refactor: route example carriers through shape isos

--- BijForm/Examples/FinChain.lean
+++ BijForm/Examples/FinChain.lean
@@ -294,7 +294,7 @@ def FinChainSyntaxShapeIso (i : Nat) : FinChainSyntax i ≃ᵢ FinChainCarrier i
 `FinChainSize`. This theorem exercises generated finite carriers with many
 different `Fin k` values rather than only a singleton finite case. -/
 def FinChainSyntaxFinIso (i : Nat) : FinChainSyntax i ≃ᵢ Fin (FinChainSize i) :=
-  GeneratedCode.shapeFinIso FinChainGeneratedCode FinChainGeneratedShapeCode i rfl
+  Iso.trans (FinChainSyntaxShapeIso i) (CodeShape.finiteIso rfl)
 
 def FinChainSyntaxFinOneIso : FinChainSyntax 0 ≃ᵢ Fin 1 :=
   FinChainSyntaxFinIso 0
--- BijForm/Examples/Sorted.lean
+++ BijForm/Examples/Sorted.lean
@@ -787,7 +787,7 @@ def SortedSyntaxShapeIso (i : SortedIx) : SortedSyntax i ≃ᵢ SortedCarrier i 
 
 def SortedSyntaxNatIsoOfBound (i : SortedIx) (h : Bound.le i.1 i.2) :
     SortedSyntax i ≃ᵢ Nat :=
-  GeneratedCode.shapeNatIso SortedGeneratedCode SortedGeneratedShapeCode i (by
+  Iso.trans (SortedSyntaxShapeIso i) (CodeShape.infiniteIso (by
     cases i with
     | mk lower upper =>
         cases upper with
@@ -795,11 +795,11 @@ def SortedSyntaxNatIsoOfBound (i : SortedIx) (h : Bound.le i.1 i.2) :
         | some upper =>
             change SortedShape (lower, some upper) = CodeShape.infinite
             dsimp [SortedShape, Bound.le] at h ⊢
-            rw [if_pos h])
+            rw [if_pos h]))
 
 def SortedSyntaxFinOneIsoOfNotBound (i : SortedIx) (h : ¬Bound.le i.1 i.2) :
     SortedSyntax i ≃ᵢ Fin 1 :=
-  GeneratedCode.shapeFinIso SortedGeneratedCode SortedGeneratedShapeCode i (by
+  Iso.trans (SortedSyntaxShapeIso i) (CodeShape.finiteIso (by
     cases i with
     | mk lower upper =>
         cases upper with
@@ -807,7 +807,7 @@ def SortedSyntaxFinOneIsoOfNotBound (i : SortedIx) (h : ¬Bound.le i.1 i.2) :
         | some upper =>
             change SortedShape (lower, some upper) = CodeShape.finite 1
             dsimp [SortedShape, Bound.le] at h ⊢
-            rw [if_neg h])
+            rw [if_neg h]))
 
 def SortedSyntaxInfiniteNatIso (lower : Nat) :
     SortedSyntax (lower, none) ≃ᵢ Nat :=
